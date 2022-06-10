58126ead795db72060df732733a6862b02c9d13b
Nicolas-Verbaere <nverbaere@gmail.com>
2022-06-10 13:51:54 +0200

    component main ok

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,16 @@
 import logo from "../assets/img/logo_bibino.png";
 import iconProfil from "../assets/img/icone_profil.png";
-import imgApropos from "../assets/img/articleapropos.jpg";
+
+
+import Main from './Main/Main'
 
 import './App.scss';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="header">
         <section className="top_header">
-          {/* <figcaption className="top_header_logo">
-            <img className="top_header_logo-png" src={logo} alt="" />
-            <h1 className="top_header_logo-title">Bibino</h1>
-          </figcaption> */}
           <img className="top_header-logo" src={logo} alt="" />
           <h1 className="top_header-title">Bibino</h1>         
           <img className="top_header-profil" src={iconProfil} alt="profil"></img>
@@ -23,32 +21,7 @@ function App() {
         </nav>
       </header>
 
-      <main className="main">
-        <article className="main_article">
-          <img className="main_article-img" src={imgApropos} alt="" />
-          <div className="main_article_rightside">
-           <h2 className="main_article_rightside-title">Venez en découvrir plus sur nous!</h2>
-           <p className="main_article_rightside-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sodales facilisis. Cras aliquam leo a lacus accumsan, vel bibendum velit feugiat. Nulla in lacinia risus. Phasellus eu nisl ante. Nulla purus erat, eleifend aliquam fringilla eu, lacinia bibendum diam. Ut pharetra eget dui at pulvinar. Phasellus eget facilisis urna.</p>
-          </div>
-
-        </article>
-
-        <article className="main_article">
-          <img src="#" alt="" />
-          <div className="main_article_rightside">
-           <h2 className="main_article_rightside-title">Découvrez les bières les mieux notées des 7 derniers jours</h2>
-           <p className="main_article_rightside-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sodales facilisis. Cras aliquam leo a lacus accumsan, vel bibendum velit feugiat. Nulla in lacinia risus. Phasellus eu nisl ante. Nulla purus erat, eleifend aliquam fringilla eu, lacinia bibendum diam. Ut pharetra eget dui at pulvinar. Phasellus eget facilisis urna.</p>
-          </div>
-        </article>
-
-        <article className="main_article">
-          <img src="#" alt="" />
-          <div className="main_article_rightside">
-           <h2 className="main_article_rightside-title">Découvrez la liste de bière et leur avis</h2>
-           <p className="main_article_rightside-content">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec sagittis sodales facilisis. Cras aliquam leo a lacus accumsan, vel bibendum velit feugiat. Nulla in lacinia risus. Phasellus eu nisl ante. Nulla purus erat, eleifend aliquam fringilla eu, lacinia bibendum diam. Ut pharetra eget dui at pulvinar. Phasellus eget facilisis urna.</p>
-          </div>
-        </article>
-      </main>
+      <Main/>
 
       <footer className="footer">
         <p className="footer-copyright">Copyright © 2022 Bibino - Tous droits réservés</p>
