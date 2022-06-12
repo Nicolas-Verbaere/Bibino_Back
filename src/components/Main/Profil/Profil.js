c5fa653d962247c054e2b7645dabe51080d2c84e
Arnaud JRGB <jaureguiberryarnaud@gmail.com>
2022-06-12 21:09:40 +0200

    fix change color of title at hover of an article and test of background image on nav

---
+++ src/components/Main/Profil/Profil.js
@@ -0,0 +1,28 @@
+import './Profil.scss';
+
+function Profil() {
+    return (
+        <>
+            <section className="profil">
+                <p className="profil-welcome">Bonjour,</p>
+                <h1 className="profil-pseudo">PSEUDO</h1>
+            </section>
+
+            <section>
+                <h1>Ici, vous pouvez gérer vos reviews.</h1>
+                <section>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                    <article></article>
+                </section>
+            </section>
+        </>
+    );
+}
+
+export default Profil;
