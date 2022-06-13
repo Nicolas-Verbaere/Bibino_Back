ca17b6a768034df38c870b66f7b256803c0daef9
Nicolas-Verbaere <nverbaere@gmail.com>
2022-06-13 14:19:43 +0200

    setting up the form

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,22 @@
 import React from 'react';
 
-import ArticleAPropos from "./ArticleAPropos/ArticleAPropos"
-import ArticleTopBeers from "./ArticleTopBeers/ArticleTopBeers"
-import Authentification from "./Authentification/Authentification"
-import './Main.scss'
+import ArticleAPropos from './ArticleAPropos/ArticleAPropos';
+import ArticleTopBeers from './ArticleTopBeers/ArticleTopBeers';
+// import Authentification from './Authentification/Authentification';
+// import AuthentificationSemanticUi from './Authentification/AuthentificationSemanticUi';
+import Form from './Authentification/Form';
 
-function Main (){
+import './Main.scss';
 
-return (
-
-    <main className="main">
-        {/* <ArticleAPropos/>
-        <ArticleTopBeers/> */}
-        <Authentification/>
-      </main>
-    )   
+function Main() {
+    return (
+        <main className="main">
+            {/* <ArticleAPropos /> */}
+            {/* <ArticleTopBeers /> */}
+            {/* <Authentification /> */}
+            {/* <AuthentificationSemanticUi /> */}
+            <Form />
+        </main>
+    );
 }
 export default React.memo(Main);
--- src/index.js
+++ src/index.js
@@ -5,11 +5,13 @@ import './index.scss';
 import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 
+// import 'semantic-ui-css/semantic.min.css';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
