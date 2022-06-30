adda2ff64ebb81f7b591e688bd178682f3d135f1
Arnaud JRGB <jaureguiberryarnaud@gmail.com>
2022-06-30 13:49:11 +0200

    fix css

--- src/components/App.js
+++ src/components/App.js
@@ -106,6 +106,7 @@ const App = () => {
             setIsLogged(true);
             getUser();
             getUserReviews();
+            getBieres();
         }
         getBieres();
     }, [isLogged]);
